4f333856edee2add71a0445b3a87d720259987b7
KevinLavigne <lavigne.kevin02@gmail.com>
2022-03-28 11:43:31 +0200

    my app

---
+++ react-intro/my-app/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+  
+  function App() {
+    return (
+      <div>
+        <img src={logo} className="App-logo" alt="logo" />
+      <h1>Welcome to my app</h1>
+      <h2>voici l'application de Clément et Kevin</h2>
+      </div>
+  );
+  }
+  
+  
+  
+  
+export default App;
+
